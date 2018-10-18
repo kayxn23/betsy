93479e546c5a43e6dc5ac6aa67c514d6aca356c3
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-18 11:49:32 -0700

    Added orders controller'

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,35 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
+    # Find user
+    # If no user, User.create 
+   if params[:user_id]
+     @user_id = params[:user_id].to_i
+     user = User.find_by(id: @user_id)
+     if user.nil?
+       flash.now[:warning] = "That user doesn't exit"
+     end
+     @order.user_id = @user_id
+   end
   end
 
   def create
-  end
+    @order = Order.new(book_params)
+     if @order.save # save returns true if the database insert succeeds
+       flash[:success] = 'Order Created!'
+
+       redirect_to root_path # go to the index so we can see the book in the list
+     else # save failed :(
+       flash.now[:danger] = 'Order not created!'
+
+       render :new, status: :bad_request # show the new book form view again
+     end
+   end
 
   def show
+  end
+
+  def order_params
+    return params.require(:order).permit(:status)
   end
 end
