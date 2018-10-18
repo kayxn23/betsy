37247a7403c7777dd9ae8ece37284e791abc340d
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-17 19:04:03 -0700

    Created order items join migration

---
+++ db/migrate/20181018020214_create_orders_items_join.rb
@@ -0,0 +1,8 @@
+class CreateOrdersItemsJoin < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders_items do |t|
+      t.belongs_to :order, index: true
+      t.belongs_to :product, index: true
+    end
+  end
+end
