f3ff124655884d21394192cbc055a5fc4bf2be06
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-17 19:55:00 -0700

    Arrange relationship in orders items join table

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ApplicationRecord
+  has_many :products, through: :orders_items
 end
--- db/migrate/20181018020214_create_orders_items_join.rb
+++ db/migrate/20181018020214_create_orders_items_join.rb
@@ -1,8 +1,8 @@
 class CreateOrdersItemsJoin < ActiveRecord::Migration[5.2]
   def change
     create_table :orders_items do |t|
-      t.belongs_to :order, index: true
-      t.belongs_to :product, index: true
+      t.belongs_to :order, index: true, foreign_key: true
+      t.belongs_to :product, index: true, foreign_key: true
     end
   end
 end
