e0f116df51eb25f3458bbb55f9512fcb3196ac47
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-18 09:37:18 -0700

    Changed product model has_many products to has_many orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :user
-  has_many :products, through: :orders_items
+  has_many :orders, through: :orders_items
 
 
 end
--- db/migrate/20181018014144_create_categorys_products_join.rb
+++ db/migrate/20181018014144_create_categorys_products_join.rb
@@ -2,7 +2,7 @@ class CreateCategorysProductsJoin < ActiveRecord::Migration[5.2]
   def change
     create_table :categories_products do |t|
       t.belongs_to :product, index: true
-      t.belongs_to :category, index: true 
+      t.belongs_to :category, index: true
     end
   end
 end
