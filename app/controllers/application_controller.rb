fcb633ffa924bade9f96c970572d6c56ebb855d6
Danielle Metzner <danimetzner@gmail.com>
2018-10-18 16:56:01 -0700

    Added is_merchant method to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :find_user
+  before_action :is_merchant?
 
   private
 
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   #   @current_user = User.find_by(id: session[:user_id])
   # end
   #
-  # def is_merchant?
-  #   return User.find_by(uid: session[:uid], provider: 'github')
-  # end
+  def is_merchant?
+    return User.find_by(id: session[:user_id], provider: 'github')
+  end
 
 
 end
