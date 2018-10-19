fa192b106fb159b8a3fac1ea5552a4f6642e005c
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-19 16:39:13 -0700

    Still working on tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -40,6 +40,27 @@ describe SessionsController do
     end
 
     it "Rejects a user with invalid data" do
+      # Arrange
+      user = users(:grace)
+      # Making use have invalid data
+      user.name = nil
+
+      # Tell OmniAuth to use this user's info when it sees
+      # an auth callback from github
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      # Act - Will not change user count
+        get auth_callback_path('github')
+
+        # Assert
+        # Won't add a user to database
+        must_change('User.count', 0)
+        # Will flash an error
+        expect(flash[:error]).must_equal "Could not create new user account: #{user.errors.messages}"
+        # And redirect to root_path
+        must_respond_with :redirect
+        must_redirect_to root_path
+
     end
   end
 end
