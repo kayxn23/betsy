abe34db879ebe0d65f18f73750815e723fd2e394
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-19 13:44:24 -0700

    Added routes/deleted destroy views

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
 
-  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
 
   resources :orders, only: [:show, :new, :create]
+
+  resources :sessions, only: [ :login, :destroy, :new ]
   # get 'sessions/login'
   # get 'sessions/destroy'
   # get 'sessions/new'
+  resources :users, only: [ :new, :create, :show]
   # get 'users/new'
   # get 'users/create'
   # get 'users/show'
-  get '/products', to: 'products#index', as: 'products'
-  get '/products/:id', to: 'products#show', as: 'product'
+  resources :products
+  # get '/products', to: 'products#index', as: 'products'
+  # get '/products/:id', to: 'products#show', as: 'product'
 
   # get 'products/show'
   # get 'products/update'
@@ -18,8 +22,10 @@ Rails.application.routes.draw do
   # get 'products/create'
   # get 'products/edit'
   # get 'products/destroy'
+  resources :categories, only: [ :new, :create ]
   # get 'categories/new'
   # get 'categories/create'
+  resources :orders, only: [ :new, :create, :show ]
   # get 'orders/new'
   # get 'orders/create'
   # get 'orders/show'
