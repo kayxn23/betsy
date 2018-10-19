0a22aa23f714b298e6aeba2d4a331408e5cbc965
Danielle Metzner <danimetzner@gmail.com>
2018-10-18 17:29:34 -0700

    Added the application controller methods find_merchant, is_merchant, set_order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,34 @@
 class ApplicationController < ActionController::Base
+  before_action :find_merchant
   before_action :is_merchant?
-
+  before_action :set_order
   private
 
-  # def find_user
-  #   @current_user = User.find_by(id: session[:user_id])
-  # end
-  #
-  def is_merchant?
-    return User.find_by(id: session[:user_id], provider: 'github')
+  def find_merchant
+    @merchant = User.find_by(id: session[:user_id], provider: 'github')
   end
+
+  def is_merchant?
+    return @merchant
+  end
+
+  def set_order
+    #Does an order exist?
+    @order = Order.find(session[:order_id])
+
+    #There is no order in the db
+    if @order.nil?
+      if @merchant #Does the order have a merchant?
+        @order = Order.create(user_id: @merchant.id, status: "pending")
+        session[:order_id] = @order.id
+      else #They are checking out as a guest, there won't be a user_id relation
+        @order = Order.create(status: "pending")
+        session[:order_id] = @order.id
+      end
+    end
+
+  end
+
 
 
 end
