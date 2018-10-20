1806311d20727e74d12b62bed2fff1df24842dcd
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-20 09:18:27 -0700

    Created user test for show

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,14 +6,35 @@ describe UsersController do
     value(response).must_be :success?
   end
 
+  describe "show" do
+    it "should get show" do
+      # Arrange
+      id = users(:grace).id
+
+      # Act
+      get user_path(id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "should respond with not_found if given an invalid id" do
+      # Arrange
+      id = -1
+
+      # Act
+      get user_path(id)
+
+      #Assert
+      must_respond_with :not_found
+      expect(flash[:danger]).must_equal "Cannot find the book -1"
+    end
+  end
+  
   it "should get create" do
     get users_create_url
     value(response).must_be :success?
   end
 
-  it "should get show" do
-    get users_show_url
-    value(response).must_be :success?
-  end
 
 end
