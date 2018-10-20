174d1ad0e9639852b14553cd875bbc3a0ed11c08
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-20 09:16:30 -0700

    Continued troubleshooting routes and sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -18,6 +18,7 @@ describe SessionsController do
       # Assert
       must_redirect_to root_path
       expect(session[:user_id]).must_equal user.id
+      must_respond_with :success
     end
 
     it "Can log in a new user with good data" do
@@ -37,24 +38,30 @@ describe SessionsController do
       # Assert
       must_redirect_to root_path
       expect(session[:user_id]).wont_be_nil
+      must_respond_with :success
     end
 
     it "Rejects a user with invalid data" do
       # Arrange
-      user = users(:grace)
+      user = users(:john)
       # Making use have invalid data
       user.name = nil
-
+      # binding.pry
       # Tell OmniAuth to use this user's info when it sees
       # an auth callback from github
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
 
       # Act - Will not change user count
         get auth_callback_path('github')
-
+        # binding.pry
         # Assert
         # Won't add a user to database
-        must_change('User.count', 0)
+
+        ##### HELP #######
+        # NoMethodError:
+        # NoMethodError: private method `binding' called for nil:NilClass
+        #Did you mean?  __binding__
+        wont_change('User.count')
         # Will flash an error
         expect(flash[:error]).must_equal "Could not create new user account: #{user.errors.messages}"
         # And redirect to root_path
@@ -63,4 +70,29 @@ describe SessionsController do
 
     end
   end
+
+  describe "logout" do
+    # Arrange
+    # Creating user / logging user in
+    user = users(:grace)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+    # Logging user in
+      get auth_callback_path('github')
+      start_count = User.count
+
+      # Act - Logging user out
+      user.destroy
+
+      # Assert
+      # Should remove a user from database
+      User.count.must_equal (start_count - 1)
+      # Flash a message
+      expect(flash[:success]).must_equal "Successfully logged out!"
+      # And redirect to root_path
+      must_respond_with :redirect
+      must_redirect_to root_path
+
+  end
+
 end
