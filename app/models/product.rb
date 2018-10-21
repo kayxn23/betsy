1d714f58023e47682ce7223e3fb4d77c16e39570
Danielle Metzner <danimetzner@gmail.com>
2018-10-21 14:08:38 -0700

    Added product tests with relationship tests and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,10 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :orders, through: :orders_items
 
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+  validates :stock, presence: true
 
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,90 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
+  let(:product) { products(:graveyard) }
 
   it "must be valid" do
-    value(product).must_be :valid?
+    expect(product).must_be :valid?
+  end
+
+  it "must have required fields" do
+    fields = [:name, :price, :description, :photo, :stock]
+
+    fields.each do |field|
+      expect(product).must_respond_to field
+    end
+  end
+
+  describe 'Relationships' do
+    it 'belongs to user' do
+      user = product.user
+
+      expect(user).must_be_instance_of User
+      expect(user.id).must_equal product.user_id
+    end
+
+    it 'can have many categories' do
+      product.categories << Category.first
+      categories = product.categories
+
+      expect(categories.length).must_be :>=, 1
+      categories.each do |category|
+        expect(category).must_be_instance_of Category
+      end
+
+    end
+
+    it 'can have many orders' do
+    end
+  end
+  describe 'Validations' do
+    it 'must have name' do
+        product = products(:haunted_house)
+        product.name = nil
+
+        valid = product.save
+        expect(valid).must_equal false
+        expect(product.errors.messages).must_include :name
+
+    end
+
+    it 'must have price' do
+      product = products(:haunted_house)
+      product.price = nil
+
+      valid = product.save
+      expect(valid).must_equal false
+      expect(product.errors.messages).must_include :price
+    end
+
+    it 'must have description' do
+      product = products(:haunted_house)
+      product.description = nil
+
+      valid = product.save
+      expect(valid).must_equal false
+      expect(product.errors.messages).must_include :description
+    end
+
+    it 'must have photo' do
+      product = products(:haunted_house)
+      product.photo = nil
+
+      valid = product.save
+      expect(valid).must_equal false
+      expect(product.errors.messages).must_include :photo
+    end
+
+    it 'must have stock' do
+      product = products(:haunted_house)
+      product.stock = nil
+
+      valid = product.save
+      expect(valid).must_equal false
+      expect(product.errors.messages).must_include :stock
+    end
+
+
+
   end
 end
