5d708fec7cc628ea75795868c3b12be5a02630b2
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-23 15:50:04 -0700

    began work on find my order items method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   has_many :orders_items, dependent: :destroy
-  has_many :products, through: :orders_items, dependent: :destroy 
+  has_many :products, through: :orders_items, dependent: :destroy
 
   belongs_to :user, optional: true
 
@@ -40,4 +40,14 @@ class Order < ApplicationRecord
 
     return current_product
   end
+
+  def my_sold_items
+    # sold items array
+    # loop through products of merchant
+    # loop through orders_items
+    # Push each item to array
+    # return the array
+    # in view, check if array is empty before trying to show things
+  end
+
 end
