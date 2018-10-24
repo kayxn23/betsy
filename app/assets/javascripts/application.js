9334000656a03a03352b6ee23e4eae97241182ef
Danielle Metzner <danimetzner@gmail.com>
2018-10-24 05:56:35 -0700

    Working on the homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,19 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+// Sticky Header
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        $(".top-nav").addClass("light-header");
+    } else {
+        $(".top-nav").removeClass("light-header");
+    }
+});
+
+// Year for copy content
+$(function(){
+var theYear = new Date().getFullYear();
+$('#year').html(theYear);
+});
