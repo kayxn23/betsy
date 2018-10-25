66203521d9771672b2cd41fc82d2bdd194cd907d
Kay <kayxn23@gmail.com>
2018-10-25 09:45:21 -0700

    added app helper def format_date to use in dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def readable_price(price)
     ("<span class='price'>" + number_with_precision(price, :precision => 2, :delimiter => ',') + "</span>").html_safe
   end
+
+  def format_date(date)
+    ("<span class='date'>" + date.strftime('%b %e, %Y')+ "</span>").html_safe
+  end
 end
